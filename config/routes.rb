3feeb6bb5aad7584dbf75da9c248da06f7e3b6a2
markpriscaru <markpriscaru@gmail.com>
2016-04-25 21:29:06 +0000

    More bootstrap goodness

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
   resources :quotes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-root 'quotes#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
